4b920b5f53e331bab69862571495d1b79ae89068
rxliuli <rxliuli@gmail.com>
2020-08-29 22:35:26 +0800

    测试环境变量, 添加 cmd-env 简化环境变量配置

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Button from '@material-ui/core/Button';
 
 interface AppProps {}
 
 function App({}: AppProps) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header>
+        <h2>App</h2>
       </header>
+      <p>环境变量：{import.meta.env.SNOWPACK_PUBLIC_IBL_ENV}</p>
+      <Button>按钮</Button>
     </div>
   );
 }
